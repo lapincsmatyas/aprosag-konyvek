dbd742d9827568bb82bdbc9bc5aa50b2204f3c2b
lapincs.matyas <lapincs.matyas@gmail.com>
2022-03-31 14:22:48 +0200

    add-to-cart-from-card

--- src/app/pages/items/item-card/item-card.component.ts
+++ src/app/pages/items/item-card/item-card.component.ts
@@ -4,6 +4,9 @@ import {ImageCacheService} from "../../../services/image-cache/image-cache.servi
 import {of} from "rxjs";
 import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 import {Router} from "@angular/router";
+import {CartService} from "../../../services/cart/cart.service";
+import {AddedToCartComponent} from "../../../shared/popups/added-to-cart/added-to-cart.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'aprosag-item-card',
@@ -14,10 +17,22 @@ export class ItemCardComponent {
   @Input()
   public item: Item = {};
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private cartService: CartService, private modalService: NgbModal) {
   }
 
   openItem() {
     this.router.navigate(['/items',this.item.id]);
+  }
+
+  addItemToCart() {
+    this.cartService.addItemToCart(this.item, 1);
+
+    const modalRef = this.modalService.open(AddedToCartComponent, {
+      backdrop: true,
+      backdropClass: 'modal-dialog-backdrop',
+      modalDialogClass: 'modal-dialog-centered added-to-cart-dialog'
+    });
+    modalRef.componentInstance.item = this.item;
+    modalRef.componentInstance.amount = 1;
   }
 }
